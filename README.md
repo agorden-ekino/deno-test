4ad5aa47b73526fb368e300544e798b4373f7258
Arthur Gorden <arthur.gorden@ekino.com>
2024-10-30 14:38:01 +0100

    feat(stuff): init dependencies vendorization and doing other stuff

--- README.md
+++ README.md
@@ -8,5 +8,13 @@ docker exec -it deno-fresh-test bash # access deno container's terminal
 docker-compose down # stop project
 ```
 
-See [deno.json file](./deno.json) for executable `deno` commands in Deno container.
+See [deno.json file](./deno.json) for executable `deno` commands in Deno
+container.
 
+In deno container, you can run:
+
+```sh
+deno run --reload my_module.ts # refetch and recompile modules
+# As we are vendoring remote modules thanks to the vendor:true config in deno.json, following command will immediately cache dependencies in the vendor:
+deno install --entrypoint main.ts # or target another file
+```
--- server.ts
+++ server.ts
@@ -1,19 +1,39 @@
-import { Application } from "@oak/oak/application";
-import { Router } from "@oak/oak/router";
+import { Application, Router } from "@oak/oak";
 
 const router = new Router();
-router.get("/", (ctx) => {
-  ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <h1>Hello oak!</h1>
-      </body>
-    </html>
-  `;
-});
+// ctx means context
+router
+  .get("/", (ctx) => {
+    ctx.response.body = `<!DOCTYPE html>
+      <html>
+        <head><title>Hello oak!</title><head>
+        <body>
+          <h1>Hello oak!</h1>
+        </body>
+      </html>
+    `;
+  })
+  .get("/plop", (ctx) => {
+    ctx.response.body = "PLOP";
+  });
 
 const app = new Application();
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
 const port = 8001;
 
 app.use(router.routes());
@@ -21,4 +41,3 @@ app.use(router.allowedMethods());
 console.log(`Server running on http://localhost:${port}`);
 
 app.listen({ port: port });
-
